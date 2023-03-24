6133b6995d7e6631a8b169a6be42302df3f245d9
antoniohidalg <a.hidalgoruiz@ufl.edu>
2023-03-23 21:07:14 -0400

    Submission of lab 6 without decode

---
+++ lab_6.py
@@ -0,0 +1,42 @@
+#Antonio
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        new_digit = str((int(digit) + 3) % 10)
+        encoded_password += new_digit
+    return encoded_password
+
+
+def main():
+    encoded_password = ""
+    while True:
+        print("\nMenu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        choice = input("\nPlease enter an option: ")
+
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!")
+
+        elif choice == "2":
+            if encoded_password == "":
+                print("Please encode a password first.")
+            else:
+                password = decode(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+
+        elif choice == "3":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
+
